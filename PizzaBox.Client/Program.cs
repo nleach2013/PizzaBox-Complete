a0d8ae6650f6184e3faf03fad3553f802cbf70ca
Noah Leach <nleach2013@fau.edu>
2021-04-16 00:01:25 -0400

    Added Order

--- PizzaBox.Client/Program.cs
+++ PizzaBox.Client/Program.cs
@@ -7,44 +7,43 @@ using PizzaBox.Client.Singletons;
 
 namespace PizzaBox.Client
 {
-    public class Program
+  public class Program
+  {
+    private static readonly StoreSingleton _storeSingleton = StoreSingleton.Instance;
+    private static void Main()
     {
-        private static readonly StoreSingleton _storeSingleton = StoreSingleton.Instance;
-        private static void Main()
-        {
-            Run();
-        }
-
-        private static void Run()
-        {
-            /*
-            var order = new Order();
-
-            //Greet
-            */
-            PrintStoreList();
-            /*
-
-            order.Customer = new Customer();
-            order.Store = SelectStore();
-            order.Pizza = SelectPizza();
-            */
-
-
-
-            sc.WriteLine("Make a selection");
-            string input = sc.ReadLine();
-            int entry = int.Parse(input);
-
-            sc.WriteLine(_storeSingleton.Stores[entry]);
-        }
-
-        private static void PrintStoreList()
-        {
-            for (var x = 0; x < _storeSingleton.Stores.Count; x += 1)
-            {
-                sc.WriteLine($"{x} {_storeSingleton.Stores[x]}");
-            }
-        }
+      Run();
     }
+
+    private static void Run()
+    {
+
+      var order = new Order();
+
+      sc.WriteLine("Welcome to PizzaBox!");
+
+      PrintStoreList();
+      /*
+      order.Customer = new Customer();
+      order.Store = SelectStore();
+      order.Pizza = SelectPizza();
+      */
+
+
+
+      sc.WriteLine("Make a selection");
+      string input = sc.ReadLine();
+      int entry = int.Parse(input);
+
+      sc.WriteLine(_storeSingleton.Stores[entry]);
+    }
+
+    private static void PrintStoreList()
+    {
+      for (var x = 0; x < _storeSingleton.Stores.Count; x += 1)
+      {
+        sc.WriteLine($"{x} {_storeSingleton.Stores[x]}");
+      }
+    }
+  }
 }
